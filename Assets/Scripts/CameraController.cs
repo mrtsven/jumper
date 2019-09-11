6417b319abbaa2588277b2016ff9924c28c3c3d7
mrtsven <zahharov@hotmail.com>
2019-09-11 12:21:26 +0200

    Can move around, funky camera controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+
+    private Vector3 offset;
+    private MovementController movement;
+
+    void Start ()
+    {
+        movement = player.GetComponent("MovementController") as MovementController;
+        offset = transform.position - player.position;
+    }
+
+    void LateUpdate ()
+    {
+        offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * movement.speed / 2 , Vector3.up) * offset;
+        transform.position = player.position + offset; 
+        transform.LookAt(player.position);
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float speed = 1;
+
+    // private Vector3 m_EulerAngleVelocity;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update(){
+        Vector3 move = new Vector3(0, 0, Input.GetAxis("Vertical")).normalized;
+        move = transform.TransformDirection(move.normalized) * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + move);
+
+        if(Input.GetAxis("Mouse X") < 0){
+            transform.Rotate(Vector3.up * speed / 2);
+        }
+        if(Input.GetAxis("Mouse X") > 0){
+            transform.Rotate(Vector3.up * -speed / 2);
+        }
+    }
+}
