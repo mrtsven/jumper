7be16ff96095c56067130db761c5217d0c1551ec
mrtsven <zahharov@hotmail.com>
2019-09-11 22:08:11 +0200

    Added platform

---
+++ Assets/Scripts/PlatformMovementController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovementController : MonoBehaviour
+{
+    public float speed = 1;
+    public float forwardFromOrigin;
+    public float backwardsFromOrigin;
+
+    private Rigidbody rb;
+    private float direction = 1;
+    private float originPosition;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        originPosition = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > originPosition + forwardFromOrigin)
+        {            
+            direction = -1;
+        } else if(transform.position.z < originPosition + backwardsFromOrigin)
+        {
+            direction = 1;
+        }
+
+        rb.MovePosition(transform.position + Vector3.forward * direction * speed);
+    }
+}
